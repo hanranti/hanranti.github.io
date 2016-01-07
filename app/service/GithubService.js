d451e613d257a1f6effea28058211c7aee40bfe9
hanranti <hanranti@users.noreply.github.com>
2016-01-07 14:44:32 +0200

    getUser

--- app/service/GithubService.js
+++ app/service/GithubService.js
@@ -3,4 +3,7 @@ GithubApp.service('GithubService', function($http){
 	this.getRepos = function(){
 	return $http.get('https://api.github.com/users/hanranti/repos');
 	}
+	this.getUser = function(){
+	return $http.get('https://api.github.com/users/hanranti');
+	}
 });
