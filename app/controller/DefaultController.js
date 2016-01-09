ededba21a9b04b6c2ec6d5031b6cb4e4fdbf8cc1
hanranti <hanranti@users.noreply.github.com>
2016-01-09 14:16:57 +0200

    Update DefaultController.js

--- app/controller/DefaultController.js
+++ app/controller/DefaultController.js
@@ -1,6 +1,3 @@
 GithubApp.controller('DefaultController', function($scope, $location, FirebaseService, GithubService){
-  GithubService.getRepos().success(function (data, status, headers, config) {
-        $scope.repos = data;
-        console.log(data);
-    });
+  FirebaseService.addVisit();
 });
