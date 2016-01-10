f15114f4637476df3eeeefad623f74a0a9414d1f
hanranti <hanranti@users.noreply.github.com>
2016-01-10 12:20:24 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var GithubApp = angular.module('hanranti', ['ngRoute']);
 GithubApp.config(function ($routeProvider) {
     $routeProvider
             .when('/', {
-                controller: 'DefaultController',
+                controller: 'ReposController',
                 templateUrl: 'app/views/Default.html'
             }).when('/repos/', {
                 controller: 'ReposController',
