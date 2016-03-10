a58e17c4062dc0d020b571aacf4f1ac69dfcf868
hanranti <hanranti@users.noreply.github.com>
2016-03-10 10:57:00 +0200

    Update DefaultController.js

--- app/controller/DefaultController.js
+++ app/controller/DefaultController.js
@@ -1,4 +1,2 @@
-GithubApp.controller('DefaultController', function($scope, $location, FirebaseService){
-  FirebaseService.addVisit();
-  $scope.visits = FirebaseService.getVisits();
+GithubApp.controller('DefaultController', function($scope){
 });
